83be924687dfb0301c891e54b05ce50004215526
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-08 00:56:10 +0900

    [ADD] just create Recipe Page

--- client/src/pages/RecipeView.vue
+++ client/src/pages/RecipeView.vue
@@ -1,21 +1,43 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import type { components } from "../schema.d.ts"
+import { onMounted, ref } from "vue"
+import type { Ref } from "vue"
+import { useRecipeState } from "../store/useRecipe.ts"
 
-const count = ref(0)
-const increment = () => {
-	count.value++
+const store = useRecipeState()
+const recipe: Ref<components["schemas"]["Recipe"] | ""> = ref("")
+
+const setRecipeToRef = () => {
+	const storedRecipe = store.getRecipe().value
+	if (storedRecipe !== "") {
+		recipe.value =  storedRecipe as components["schemas"]["Recipe"]
+	}
 }
+
+onMounted(setRecipeToRef)
 </script>
 <template>
 	<main>
 		<h1>RecipeView.vue</h1>
-		<div></div>
-		<button @click="increment">Push!!</button>
-		<h2>{{ count }}</h2>
-
 		<button>
 			<RouterLink to="/ranking">Go to Ranking</RouterLink>
 		</button>
+		<h2 v-if="recipe">
+			{{ recipe.name }}
+			<img :src="recipe.thumbnail" alt="">
+
+			<div class="recipeTextArea">
+				{{ recipe.instructions }}
+			</div>
+
+			<div class="recipeIngredients">
+				<div v-for="(ingredientQuantity, index) in recipe.ingredientQuantities" :key="index">
+					<p>
+						{{ ingredientQuantity.ingredient.name }} : {{ ingredientQuantity.quantity }}
+					</p>
+				</div>
+			</div>
+		</h2>
 	</main>
 </template>
 
