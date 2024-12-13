db679d7b682f48af748352dc4bde96ccb952fb08
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-13 11:58:11 +0900

    [UPDATE] devide RecipeCard component

---
+++ client/src/components/RecipeCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+interface Props {
+	linkPageName: string
+	linkId: string,
+	rankingNum: number,
+	imagePath: string,
+	title: string,
+}
+const props = defineProps<Props>()
+
+interface Emits {
+	(event: 'recipe-img-click'): void
+}
+const emits = defineEmits<Emits>()
+
+const handleImgClick = () => {
+	emits('recipe-img-click')
+}
+
+</script>
+
+<template>
+	<div class="recipe-card">
+		<RouterLink 
+			@click="handleImgClick"
+			:to="{
+				name: 'recipe', 
+				params: { id: props.linkId }
+			}"
+		>
+			<img class="recipe-card__keyvisual" :src="props.imagePath" :alt="title" />
+		</RouterLink>
+		<p class="recipe-card__title">
+			{{ props.title }}
+		</p>
+	</div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
