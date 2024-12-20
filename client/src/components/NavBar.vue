30bea0f6923f039d5f4e36594813b3707d7c562f
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-20 18:29:06 +0900

    [ADD] oishi ninja logo to NavBar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,13 +1,21 @@
+<script setup lang="ts">
+import appIcon from '../assets/app_icon.png'
+
+</script>
 <template>
 	<nav class="nav">
 		<RouterLink to="/" class="nav__home-link">Oishi Ninja</RouterLink>
+		<img :src="appIcon" alt="app icon" width="54px" height="46px"/>
 	</nav>
 </template>
 <style lang="css" scoped>
 .nav {
+	display: flex;
 	height: 80px;
 	text-align: center;
 	align-content: center;
+	align-items: center;
+	justify-content: center;
 	background-color: var(--theme-color);
 }
 .nav__home-link {
